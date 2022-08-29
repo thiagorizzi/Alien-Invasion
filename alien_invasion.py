be68c3e920f6078094e80a3cbd46deaa0b4ef69a
thiagorizzi <thiagorizzi97@gmail.com>
2022-08-29 14:19:09 -0300

    Making the Aliens move

--- alien_invasion.py
+++ alien_invasion.py
@@ -102,6 +102,27 @@ class AlienInvasion:
         if event.key == pygame.K_LEFT:
             # Stop the ship from moving
             self.ship.moving_left = False
+
+    def _update_aliens(self):
+        """
+        Check if the fleet is at an edge,
+        then update the positions of all aliens in the fleet.
+        """
+        self._check_fleet_edges()
+        self.aliens.update()
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+            self.settings.fleet_direction *= -1
                     
     def run_game(self):
         """Start the main loop for the game."""
@@ -109,6 +130,7 @@ class AlienInvasion:
             self._check_events_()
             self._update_screen()
             self.ship.update()
+            self._update_aliens()
             self.bullets.update()
 
             # Get rid of bullets that have disappeared.
--- settings.py
+++ settings.py
@@ -9,7 +9,13 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_speed = 1
+        self.ship_speed = 1.0
+
+        # Alien settings
+        self.alien_speed = 0.5
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
 
         # Bullet settings
         self.bullet_speed = 1.0
