cb8f5d547d74eeae9833b45a90c7f9741d6322c9
thiagorizzi <thiagorizzi97@gmail.com>
2022-09-01 22:27:40 -0300

    Bullet/Alien collision fixed

--- alien_invasion.py
+++ alien_invasion.py
@@ -118,7 +118,7 @@ class AlienInvasion:
         """Drop the entire fleet and change the fleet's direction."""
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
-            self.settings.fleet_direction *= -1
+        self.settings.fleet_direction *= -1
                     
     def run_game(self):
         """Start the main loop for the game."""
