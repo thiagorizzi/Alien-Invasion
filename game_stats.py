8fc1958adb1a3ad583ecd1ee92707e3bd9a33d8e
thiagorizzi <thiagorizzi97@gmail.com>
2022-09-13 14:44:08 -0300

    High scores added

--- game_stats.py
+++ game_stats.py
@@ -5,8 +5,11 @@ class GameStats:
         """Initialize statistics."""
         self.settings = ai_game.settings
         self.reset_stats()
+        # High score should never be reset.
+        self.high_score = 0
         # Start Alien Invasion in an active state.
         self.game_active = False
+        
 
 
     def reset_stats(self):
